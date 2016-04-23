1b7eb1a521e9bfaa1952a851670c10640876d299
nateperry <thenateperry@gmail.com>
2016-04-23 00:04:35 -0400

    changed transactions key

--- public/javascripts/src/Transactions.jsx
+++ public/javascripts/src/Transactions.jsx
@@ -60,7 +60,7 @@ module.exports = React.createClass({
         <tbody>
           <Transaction_row isNew={true} transaction={this.state.newTransaction} owner={this.state.owner} onUpdate={this.onUpdate} key={moment().unix()} />
           {this.state.transactions.map(function (row, index) {
-            return <Transaction_row transaction={row} owner={_self.state.owner} onUpdate={_self.onUpdate} onCancel={_self.resetTransaction} key={moment().unix()} />;
+            return <Transaction_row transaction={row} owner={_self.state.owner} onUpdate={_self.onUpdate} onCancel={_self.resetTransaction} key={index} />;
           })}
         </tbody>
       </table>
